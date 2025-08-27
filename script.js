40aed7ec962416f11637eb60bca361231def0666
njorrey <njorogevictorr@gmail.com>
2025-08-27 20:25:15 +0300

    added a functional and responsive background image

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+// section for the backgroundVideo speed
+    const bgVideo = document.getElementById('backgroundVideo');
+    bgVideo.playbackRate = 0.5;
+
+
+//   section for the typed.js
+var typed = new Typed("#text", {
+    strings: ["@NjorogeVictor", ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+}
+);
+
+//DARk AND LIGHT MODE TOOGLE
+const darkLight =document.getElementById(`darkLight`)
+
+
+darkLight.addEventListener(`click` ,()=>{
+    document.body.classList.toggle(`darkmode`)
+
+    if(document.body.classList.contains(`darkmode`)){
+        darkLight.innerHTML = `<i class="bi bi-sun"></i> `
+    }else{
+        darkLight.innerHTML= `<i class="bi bi-moon"></i> `
+    }
+
+})
+
